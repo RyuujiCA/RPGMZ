6362ff5090150470e7caf8dfbb4d5b8575db1876
Artille <66515110+RyuujiCA@users.noreply.github.com>
2024-07-21 16:38:35 +0200

    Update Ryu_Core.js

--- Ryu_Core.js
+++ Ryu_Core.js
@@ -1,12 +1,13 @@
 //================================================================
 // * Plugin Name    : Ryu_Core
 // - Last updated   : 19/07/2024
-// - Curent Version : 0.1.04b
+// - Curent Version : 0.1.05
 //================================================================
 /*:
  * @target MZ
- * @plugindesc V0.1.04b Core plugin providing essential classes and functions for other Ryu plugins.
+ * @plugindesc V0.1.05 Core plugin providing essential classes and functions for other Ryu plugins.
  * @author Ryuuji
+ * @url https://github.com/RyuujiCA/RPGMZ
  * 
  * @param separator1
  * @text Auto Update
@@ -18,9 +19,6 @@
  * @desc Update Ryu_Core file automatically through github
  * @parent separator1
  * 
- * @param separator2
- * @text Set Aliases
- * @desc Create custom aliases for popular methods such as V for $gameVariables
  * 
  * @command SetAlias
  * @text Set Alias
@@ -32,27 +30,39 @@
  * @option switch
  * @option actor
  * @option class
+ * @option gameMessage
  * @default variable
  * @text Type
  * @desc The type of game object to alias.
- * @parent separator2
  *
  * @arg alias
  * @type string
  * @default V
  * @text Alias
  * @desc The alias to use for the game object.
- * @parent separator2
  * 
  * @help
- * This is the core plugin for Ryu plugins. It must be installed and placed above all other Ryu plugins.
+ * This is the core plugin for Ryu plugins. 
+ * It must be installed and placed above all other Ryu plugins.
  * Will be automatically updated through github from now on.
+ * 
+ * The core plugin allows to overwrite game objects call and simplifies the 
+ * syntaxes.
+ * Call the command "SetAlias" and chose "variable", then set V (for example)
+ * You will now be able to write v.value() and v.setValue() instead of 
+ * $gameVariables.value() and $gameVariables.setValue()
+ * 
+ * For a list of syntaxes used go to : github link to syntaxes
+ * 
  * 
  */
 
+// Declare
 var Ryu = Ryu || {};
 Ryu.Core = Ryu.Core || {};
+Ryu.Core.Git = {name: 'Ryu_Core', url: 'https://raw.githubusercontent.com/RyuujiCA/RPGMZ/main/Ryu_Core.js'} || {};  // TODO implement cleaner logic
 
+// ¨Parameter exist?
 if (typeof PluginManager !== 'undefined' && PluginManager.parameters) {
     Ryu.Core.parameters = PluginManager.parameters('Ryu_Core');
 } else {
@@ -61,11 +71,11 @@ if (typeof PluginManager !== 'undefined' && PluginManager.parameters) {
 
 
 /* ========================================================================== */
-/*                                AUTO UPDATE                                 */
+/*                                 PARAMETERS                                 */
 /* ========================================================================== */
 Ryu.Core.AutoUpdate = Ryu.Core.parameters['AutoUpdate'] === 'true';
 
-Ryu.Core.UpdateScripts = function() {
+/*Ryu.Core.UpdateScripts = function() {
     const scripts = [
         { name: 'Ryu_Core', url: 'https://raw.githubusercontent.com/RyuujiCA/RPGMZ/main/Ryu_Core.js' },
         //{ name: 'Ryu_ChangeBG', url: 'https://raw.githubusercontent.com/username/RPGMakerMZ-Scripts/main/Ryu_ChangeBG.js' },
@@ -120,7 +130,7 @@ Ryu.Core.UpdateScripts = function() {
 
     // Call the update function when the game starts
     updateScripts();
-};
+};*/
 
 /* ========================================================================== */
 /*                             CORE UTILITIES                                 */
@@ -136,6 +146,67 @@ Ryu.Utils = {
         return date.toISOString();
     },
 
+    // Auto update function for scripts
+    update: function (script, link) {
+        //let name = script;
+        //let url = link;
+
+        const scripts = [
+            { name: script, url: link },
+            //{ name: 'Ryu_ChangeBG', url: 'https://raw.githubusercontent.com/username/RPGMakerMZ-Scripts/main/Ryu_ChangeBG.js' },
+            //{ name: 'Ryu_AfkTimeCollector', url: 'https://raw.githubusercontent.com/RyuujiCA/RPGMZ/main/Ryu_AfkTimeCollector.js' }
+        ];
+
+        function downloadScript(script) {
+            return new Promise((resolve, reject) => {
+                const xhr = new XMLHttpRequest();
+                xhr.open('GET', script.url, true);
+                xhr.onload = function () {
+                    if (xhr.status >= 200 && xhr.status < 300) {
+                        resolve(xhr.responseText);
+                    } else {
+                        reject(new Error(`Failed to download ${script.name}: ${xhr.statusText}`));
+                    }
+                };
+                xhr.onerror = function () {
+                    reject(new Error(`Network error while downloading ${script.name}`));
+                };
+                xhr.send();
+            });
+        }
+
+        function updateScripts() {
+            let updated = false;
+            const promises = scripts.map(script => downloadScript(script).then(content => {
+                const filename = `${script.name}.js`;
+                const path = require('path');
+                const fs = require('fs');
+                const scriptPath = path.join(process.cwd(), 'js/plugins', filename);
+                const currentContent = fs.existsSync(scriptPath) ? fs.readFileSync(scriptPath, 'utf8') : null;
+
+                if (currentContent !== content) {
+                    fs.writeFileSync(scriptPath, content);
+                    console.log(`Updated ${filename}`);
+                    updated = true;
+                }
+            }));
+
+            Promise.all(promises).then(() => {
+                if (updated) {
+                    console.log('Scripts updated successfully.');
+                    // TODO : Reload the game after update scripts
+                } else {
+                    console.log('All scripts are already up to date.');
+                }
+            }).catch(error => {
+                console.error(`Error updating scripts: ${error.message}`);
+            });
+        }
+
+        // Call the update function when the game starts
+        updateScripts();
+    },
+
     // Function to parse notetags
     parseNotetags: function (note, tag) {
         const regex = new RegExp(`<${tag}>([\\s\\S]*?)<\\/${tag}>`, 'g');
@@ -145,7 +216,26 @@ Ryu.Utils = {
             matches.push(match[1].trim());
         }
         return matches;
+    },
+
+    parseCustomNotetags: function () {
+        // Test with data items
+        // Todo Scale this up!
+        $dataItems.forEach(item => {
+            if (item && item.note) {
+                const names = Ryu.Utils.parseNotetags(item.note, 'setName');
+                names.forEach(name => {
+                    Ryu.Utils.log(`Item ${item.id} has name: ${name}`);
+                });
+
+                const strengths = Ryu.Utils.parseNotetags(item.note, 'Strength');
+                strengths.forEach(strength => {
+                    Ryu.Utils.log(`Item ${item.id} has strength: ${strength}`);
+                });
+            }
+        });
     }
+
     // More utility functions...
 };
 
@@ -177,11 +267,35 @@ Ryu.BaseWindow = class {
     // More base methods...
 };
 
-// Modified commands and methods
-PluginManager.registerCommand('Ryu_CORE', 'SetAlias', args => {
+/* ========================================================================== */
+/*                             AUTO UPDATE FILES                              */
+/* ========================================================================== */
+if (Ryu.Core.AutoUpdate === true) {
+    //Ryu.Core.UpdateScripts();
+    Ryu.Utils.update(Ryu.Core.Git.name, Ryu.Core.Git.url);
+} else {
+    Ryu.Utils.log("Auto Update inactive!");
+}
+
+
+/* ========================================================================== */
+/*                             CORE SPECIFIC PLUGIN                           */
+/* ========================================================================== */
+// Hook into DataManager to call parseCustomNotetags when data is loaded
+const _DataManager_onLoad = DataManager.onLoad;
+DataManager.onLoad = function (object) {
+    _DataManager_onLoad.call(this, object);
+    //TODO : Add all classes
+    if (object === $dataItems) {
+        Ryu.Utils.parseCustomNotetags();
+    }
+};
+
+PluginManager.registerCommand('Ryu_Core', 'SetAlias', args => {
     const type = args.type;
     const alias = args.alias;
 
+    Ryu.Utils.log(`Setting alias for ${type} as ${alias}`);
     switch (type) {
         case 'variable':
             window[alias] = {
@@ -195,26 +309,30 @@ PluginManager.registerCommand('Ryu_CORE', 'SetAlias', args => {
                 setValue: (id, val) => $gameSwitches.setValue(id, val)
             };
             break;
+        case 'gameMessage':
+            window[alias] = {
+                setFaceImg: (faceName, faceIndex) => $gameMessage.setFaceImage(faceName, faceIndex),
+                setBg: (bgType) => $gameMessage.setBackground(bgType),
+                setPos: (posType) => $gameMessage.setPositionType(posType),
+                setName: (speakerName) => $gameMessage.setSpeakerName(speakerName),
+                setNamePos: (speakerName, posType) => {
+                    $gameMessage.setSpeakerName(speakerName);
+                    $gameMessage.setPositionType(posType);
+                },
+                setMsg: (text) => $gameMessage.add(text)
+            };
+            break;
         // Add more cases for other types such as actors, classes, etc.
         default:
             throw new Error(`Unknown type: ${type}`);
     }
 });
 
-/* ========================================================================== */
-/*                             AUTO UPDATE FILES                              */
-/* ========================================================================== */
-if (Ryu.Core.AutoUpdate === true) {
-    Ryu.Core.UpdateScripts();
-} else {
-    Ryu.Utils.log("Auto Update inactive!");
-}
-
 // Example of registering a command to set variable alias V
-/*PluginManager.registerCommand('Ryu_CORE', 'SetAliasVariable', args => {
+PluginManager.registerCommand('Ryu_Core', 'SetAliasVariable', args => {
     const alias = args.alias || 'V';
     window[alias] = {
         value: (id) => $gameVariables.value(id),
         setValue: (id, val) => $gameVariables.setValue(id, val)
     };
-});*/
+});
